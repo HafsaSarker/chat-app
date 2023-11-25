ef894e39575699b4830a01fd468740c59d4c7bf5
HafsaSarker <hafsahaqs@gmail.com>
2023-11-24 19:52:59 -0500

    fix config

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/app";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+const app = firebase.initializeApp({
   apiKey: "AIzaSyAWFcsHOwRjrW8kUFN8xZMFGqdXnSaTC7w",
   authDomain: "kiki-chat-576b2.firebaseapp.com",
   projectId: "kiki-chat-576b2",
   storageBucket: "kiki-chat-576b2.appspot.com",
   messagingSenderId: "188507463023",
   appId: "1:188507463023:web:e1009e76e5d9f551945c9a",
-};
+});
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
+export const auth = app.auth();
+export default app;
