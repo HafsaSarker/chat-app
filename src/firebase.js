41b8fd4233c00f8450ea0e3533e81a346a126321
Zaara Sarker <hafsahaqs@gmail.com>
2023-12-21 22:54:05 -0500

    protect firebase config variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAWFcsHOwRjrW8kUFN8xZMFGqdXnSaTC7w",
-  authDomain: "kiki-chat-576b2.firebaseapp.com",
-  projectId: "kiki-chat-576b2",
-  storageBucket: "kiki-chat-576b2.appspot.com",
-  messagingSenderId: "188507463023",
-  appId: "1:188507463023:web:e1009e76e5d9f551945c9a",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
-
+ 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
